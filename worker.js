b2c3400e23c8e0f4bc91a0941258dd4184418440
Eric Lindgren <eric@bit.io>
2022-11-30 22:12:47 -0700

    removes experiments

--- worker.js
+++ worker.js
@@ -5,54 +5,23 @@ self.addEventListener("connect", function (e) {
 
 	port.addEventListener("message", function (e) {
 		if (e.data.file) {
-			// console.log('reveived file, beginning xhr', e.data.file)
-			//
-			// const xhr = new XMLHttpRequest()
-			// xhr.addEventListener("load", () => {
-			// 	console.log("request finished")
-			// })
-			// xhr.addEventListener("error", () => {
-			// 	console.error("request errored")
-			// })
-			// xhr.addEventListener("progress", () => {
-			// 	console.log("progress...")
-			// })
-			//
-			// xhr.open("PUT", "http://httpbin.org/put")
+			console.log('received file, beginning xhr', e.data.file)
 
-			// var byteReader = new FileReader();
-			// byteReader.onload = function (e) {
-			// 	console.log(e)
-			// 	const bytes = e.target.result
-			// 	console.log(bytes)
-			// 	xhr.send(bytes)
-			// };
-			// byteReader.readAsArrayBuffer(e.data.file);
-			// xhr.send(e.data.file)
-			const stream = e.data.file.stream()
-			console.log(stream)
-			// stream.on("data", () => console.log("streamed some data"))
-			const req = new Request('', {
-				method: "PUT",
-				headers: {'Content-Type': 'text/plain'},
-				body: stream,
-				duplex: 'half'
-				// get duplex() {
-				// 	duplexAccessed = true;
-				// 	return 'half';
-				// },
+			const xhr = new XMLHttpRequest()
+
+			xhr.addEventListener("load", () => {
+				console.log("request finished")
+			})
+			xhr.addEventListener("error", () => {
+				console.error("request errored")
+			})
+			xhr.addEventListener("progress", () => {
+				console.log("progress...")
 			})
 
-			fetch(url, {
-				method: 'GET',
-				headers: {'X-HTTP-Method-Override': 'HEAD'},
-				followRedirects: true  // Default is true anyway.
-			});
+			xhr.open("PUT", "http://httpbin.org/put")
 
-
-
-			console.log({req})
-			void fetch("http://httpbin.org/put", req).then(r => r.json()).then(r => console.log('done', r))
+			xhr.send(e.data.file)
 		}
 	}, false);
 
